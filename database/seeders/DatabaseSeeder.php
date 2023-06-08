2d35c04bea5bbf168f253397e59afb219776f4b1
Casper Kizewski <163021@student.horizoncollege.nl>
2023-06-08 13:55:39 +0200

    Fixed?

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->firstOrCreate([
+        User::firstOrCreate([
             'name' => 'Casper',
             'email' => '163021@student.horizoncollege.nl',
             'gitlab' => 'casperkiewski',
